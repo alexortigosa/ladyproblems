bf58961a25b3682b4774204d7b49bea0e7d9e500
Alexandre Ortigosa Barragan <alexandre.ortigosa@gmail.com>
2016-09-10 17:07:09 +0200

    WebCom
    WebComweciL

---
+++ README.md
@@ -0,0 +1,4 @@
+alexweb
+=======
+
+A Symfony project created on September 4, 2016, 1:14 pm.
---
+++ src/Alex/StaticPagesBundle/AlexStaticPagesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Alex\StaticPagesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AlexStaticPagesBundle extends Bundle
+{
+}
