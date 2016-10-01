a257af26a7387f664b1a4efa1df2419bcc2b9a53
alexandre <alexandre.ortigosa@gmail.com>
2016-10-01 18:27:20 +0200

    new menu

--- web/assets/boots/js/bootstrap.js
+++ web/assets/boots/js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=211d288d1f4248603237bb56aa27da8c)
- * Config saved to config.json and https://gist.github.com/211d288d1f4248603237bb56aa27da8c
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=a590dcbe81a2760fd7d9bdf11a14bc29)
+ * Config saved to config.json and https://gist.github.com/a590dcbe81a2760fd7d9bdf11a14bc29
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
